43f51774d4e532a43e57da110fe1b798f0328054
Peter Brown <peterbrn@umich.edu>
2016-05-04 14:53:22 -0400

    python-gdsii test

--- sandbox/gds_sandbox.py
+++ sandbox/gds_sandbox.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python
 # Let's play with gds import/export...
+
+from gdsii.library import Library
+from gdsii.elements import *
+
+from print_gds import *
+
+OUT('Reading file "IFneuron.gds"...')
+with open('IFneuron.gds', 'rb') as stream:
+    lib = Library.load(stream)
+OUT('Done.')
+OUT('')
+
+print_gds_library(lib)
+
