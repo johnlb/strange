ebbb45525347f132352f2711ea53891684c8c5a9
John Lappy <john.l.bell@gmail.com>
2016-05-04 18:05:13 -0400

    switched to gdspy. works with Python 3.5 (with compilation), very nice feature set, actively being maintained.

---
+++ sandbox/gdscad.py
@@ -0,0 +1,33 @@
+# Let's play with gds import/export...
+
+# 1. gdsCAD isn't python 3.x compatible, so I'm using 2.7
+# 2. there's a bug in core.py:
+# line 105 should be:
+# 	colors += list(matplotlib.cm.gist_ncar(np.linspace(0.98, 0, 15)))
+
+
+import os.path
+from gdsCAD import *
+
+# Create some things to draw:
+amarks = templates.AlignmentMarks(('A', 'C'), (1,2))
+text = shapes.Label('Hello\nworld!', 200, (0, 0))
+box = shapes.Box((-500, -400), (1500, 400), 10, layer=2)
+
+# Create a Cell to hold the objects
+cell = core.Cell('EXAMPLE')
+cell.add([text, box])
+cell.add(amarks, origin=(-200, 0))
+cell.add(amarks, origin=(1200, 0))
+
+# Create two copies of the Cell
+top = core.Cell('TOP')
+cell_array = core.CellArray(cell, 1, 2, (0, 850))
+top.add(cell_array)
+
+# Add the copied cell to a Layout and save
+layout = core.Layout('LIBRARY')
+layout.add(top)
+layout.save('./outputs/output.gds')
+
+layout.show()
--- sandbox/gdspy_tutorial.py
+++ sandbox/gdspy_tutorial.py
@@ -1,4 +1,24 @@
-# Let's start from the gdspy example and see what we can do
+########################################################################
+##                                                                    ##
+##  Copyright 2009-2016 Lucas Heitzmann Gabrielli                     ##
+##                                                                    ##
+##  This file is part of gdspy.                                       ##
+##                                                                    ##
+##  gdspy is free software: you can redistribute it and/or modify it  ##
+##  under the terms of the GNU General Public License as published    ##
+##  by the Free Software Foundation, either version 3 of the          ##
+##  License, or any later version.                                    ##
+##                                                                    ##
+##  gdspy is distributed in the hope that it will be useful, but      ##
+##  WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     ##
+##  GNU General Public License for more details.                      ##
+##                                                                    ##
+##  You should have received a copy of the GNU General Public         ##
+##  License along with gdspy.  If not, see                            ##
+##  <http://www.gnu.org/licenses/>.                                   ##
+##                                                                    ##
+########################################################################
 
 import numpy
 import gdspy
@@ -159,7 +179,7 @@ polypath = gdspy.PolyPath(points, widths, number_of_paths=5,
 
 ## We can round the corners of any Polygon or PolygonSet with the fillet
 ## method. Here we use a radius of 0.2.
-# polypath.fillet(0.2)
+#polypath.fillet(0.2)
 path_cell.add(polypath)
 
 ## L1Paths use only segments in 'x' and 'y' directions, useful for some
